82f0ef7c319f0131926461daaf0230c70651ae19
rymdhund <rymdhund@forskningsavd.se>
2013-12-14 14:53:54 +0100

    encoding fix

--- htdocs/query.php
+++ htdocs/query.php
@@ -1,7 +1,7 @@
 <?php
 function myLog($str){
     $str .= "\n";
-    //file_put_contents("../log/query.log", $str, FILE_APPEND);
+    file_put_contents("../log/query.log", $str, FILE_APPEND);
 }
 function search($term){
     $db = openDb();
@@ -54,12 +54,15 @@ if(array_key_exists('term', $_GET)){
     myLog("searching $term");
 
     $res = search($term);
-
     echo json_encode($res);
 }else if(array_key_exists('title', $_GET)){
-    $title = utf8_encode($_GET['title']);
+    $title = $_GET['title'];
+    if(mb_detect_encoding($_GET['title'], "UTF-8, ISO-8859-1") != "UTF-8"){
+        $title = utf8_encode($title);
+    }
     myLog("getting keywords for $title");
-    echo json_encode(makeBoard($title));
+    $board = makeBoard($title);
+    echo json_encode($board);
 }else{
     echo json_encode(Array());
 }
