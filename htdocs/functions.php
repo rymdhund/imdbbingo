ef884ba18619949dcfa8690046b7293ec606f6b9
rymdhund <rymdhund@forskningsavd.se>
2014-09-20 15:29:21 +0200

    2space indents

--- htdocs/functions.php
+++ htdocs/functions.php
@@ -1,65 +1,68 @@
 <?php
 
 function myLog($str){
-    $str .= "\n";
-    file_put_contents("../log/query.log", $str, FILE_APPEND);
+  $str .= "\n";
+  file_put_contents("../log/query.log", $str, FILE_APPEND);
 }
+
 function search($term){
-    $db = openDb();
-    if($db === null){
-        myLog("could not open db");
-        return Array();
-    }
-    // simple full text search
-    $terms = explode(' ', $term);
+  $db = openDb();
+  if($db === null){
+    myLog("could not open db");
+    return Array();
+  }
+  // simple full text search
+  $terms = explode(' ', $term);
 
-    $db_terms = array();
-    foreach($terms as $t){
-        $db_terms[] = sprintf('name LIKE \'%%%s%%\'', $db->escapeString($t));
-    }
+  $db_terms = array();
+  foreach($terms as $t){
+    $db_terms[] = sprintf('name LIKE \'%%%s%%\'', $db->escapeString($t));
+  }
 
-    $sql = sprintf('SELECT name FROM movies WHERE %s limit 10', implode(' and ', $db_terms));
-    $q = $db->query($sql);
-    $res = Array();
-    while($ret = $q->fetchArray()){
-        $res[] = $ret[0];
-    }
-    $db->close();
-    return $res;
+  $sql = sprintf('SELECT name FROM movies WHERE %s limit 10', implode(' and ', $db_terms));
+  $q = $db->query($sql);
+  $res = Array();
+  while($ret = $q->fetchArray()){
+    $res[] = $ret[0];
+  }
+  $db->close();
+  return $res;
 }
+
 function makeBoard($title){
-    $db = openDb();
-    if($db === null){
-        myLog("could not open db");
-        return Array();
-    }
-    $sql = sprintf('SELECT kw FROM keywords WHERE name = \'%s\'', $db->escapeString($title));
-    $q = $db->query($sql);
-    $kws = Array();
-    while($ret = $q->fetchArray()){
-        $kws[] = $ret[0];
-    }
-    //shuffle($kws);
-    $db->close();
+  $db = openDb();
+  if($db === null){
+    myLog("could not open db");
+    return Array();
+  }
+  $sql = sprintf('SELECT kw FROM keywords WHERE name = \'%s\'', $db->escapeString($title));
+  $q = $db->query($sql);
+  $kws = Array();
+  while($ret = $q->fetchArray()){
+    $kws[] = $ret[0];
+  }
+  //shuffle($kws);
+  $db->close();
 
-    if(count($kws) < 9)
-      return array();
+  if(count($kws) < 9)
+    return array();
 
-    $rand_keys = array_rand($kws, 9);
-    $res = Array();
-    if($rand_keys !== null){
-        foreach($rand_keys as $n){
-            $res[] = str_replace('-', ' ', $kws[$n]);
-        }
+  $rand_keys = array_rand($kws, 9);
+  $res = Array();
+  if($rand_keys !== null){
+    foreach($rand_keys as $n){
+      $res[] = str_replace('-', ' ', $kws[$n]);
     }
-    return $res;
+  }
+  return $res;
 }
+
 function openDb(){
-    $db = new SQLite3('../keywords.db');
-    if ($db){
-        return $db;
-    }
-    return null;
+  $db = new SQLite3('../keywords.db');
+  if ($db){
+    return $db;
+  }
+  return null;
 }
 
 ?>
--- htdocs/query.php
+++ htdocs/query.php
@@ -2,21 +2,21 @@
 require_once('functions.php');
 
 if(array_key_exists('term', $_GET)){
-    $term = $_GET['term'];
-    myLog("searching $term");
+  $term = $_GET['term'];
+  myLog("searching $term");
 
-    $res = search($term);
-    echo json_encode($res);
+  $res = search($term);
+  echo json_encode($res);
 }else if(array_key_exists('title', $_GET)){
-    $title = $_GET['title'];
-    if(mb_detect_encoding($_GET['title'], "UTF-8, ISO-8859-1") != "UTF-8"){
-        $title = utf8_encode($title);
-    }
-    myLog("getting keywords for $title");
-    $board = makeBoard($title);
-    echo json_encode($board);
+  $title = $_GET['title'];
+  if(mb_detect_encoding($_GET['title'], "UTF-8, ISO-8859-1") != "UTF-8"){
+    $title = utf8_encode($title);
+  }
+  myLog("getting keywords for $title");
+  $board = makeBoard($title);
+  echo json_encode($board);
 }else{
-    echo json_encode(Array());
+  echo json_encode(Array());
 }
 
 ?>
